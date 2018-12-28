a84c606c00ea511bfc3c8d5b7e6a7e232a09585f
Thinkerk <53430806@qq.com>
2018-12-28 16:45:02 +0800

    errorPage

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,16 @@ import { _import } from '../utils/common' //模块加载方法
 import businessRouter from "./business_router.js"	//业务模块路由
 
 const routerModule = [{
+	path: '*',
+	redirect: '/404',
+	hidden: true
+}, {
+	path: '/401',
+	component: _import('errorPage/401'),
+}, {
+	path: '/404',
+	component: _import('errorPage/404'),
+}, {
 	path: "/login",
 	name: 'login',
 	component: _import("login/login")
